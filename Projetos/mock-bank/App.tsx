8150b3c152d31ba01fede1dfdf2600900668d506
Samir Neto <samihneto@gmail.com>
2025-04-23 11:57:27 -0300

    Mais mudanças

--- Projetos/mock-bank/App.tsx
+++ Projetos/mock-bank/App.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
-import { AuthRoutes, AuthenticatedRoutes } from "../navigation/stack.routes";
+import { AuthRoutes, AuthenticatedRoutes } from "./src/navigation/stack.routes";
 
 export default function App() {
     const isAuthenticated = false;
